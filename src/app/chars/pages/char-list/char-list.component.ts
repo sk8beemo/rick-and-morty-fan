aa46a96cd0f8725b1e5483130d3dd3d3f86ff22a
Натаров Евгений <natarover@it2g.ru>
2022-04-19 17:51:31 +0300

    [sk8beemo] codegen types

--- src/app/chars/pages/char-list/char-list.component.ts
+++ src/app/chars/pages/char-list/char-list.component.ts
@@ -1,15 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject, map, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-char-list',
   templateUrl: './char-list.component.html',
-  styleUrls: ['./char-list.component.scss']
+  styleUrls: ['./char-list.component.scss'],
 })
 export class CharListComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  public ngOnInit(): void {}
 }
